826d53aea19854d474e62275771196db71129b05
GH_NoteBook <elfgh29@naver.com>
2025-08-27 18:22:20 +0900

    공격 로직 틀 구현

--- Game/Assets/Scripts/Player/PlayerController.cs
+++ Game/Assets/Scripts/Player/PlayerController.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    private float moveX;
     private Rigidbody2D rb;
     private Animator animator;
-    private float moveX;
+
+    private bool isAttack = false;
+
+    public GameObject projectilePrefab;
+    public Transform firePoint;
+
 
     void Start()
     {
@@ -17,22 +23,33 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // 좌/우 입력 받기 (A/D 키 or 화살표 ← →)
-        moveX = Input.GetAxisRaw("Horizontal");
+        if (!isAttack)
+            Move();
 
-        // 애니메이션 제어 (이동 여부)
+        if (Input.GetKeyDown(KeyCode.Space))
+            Attack();
+    }
+
+    void Move()
+    {
+        moveX = Input.GetAxisRaw("Horizontal");
         animator.SetBool("isWalk", moveX != 0);
 
-        // 방향 반전 처리 (왼쪽 이동 시 캐릭터 flip)
         if (moveX != 0)
         {
             transform.localScale = new Vector3(-Mathf.Sign(moveX), 1, 1);
         }
     }
 
+    void Attack()
+    {
+        isAttack = true;
+        animator.SetTrigger("doAttack");
+        // Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+    }
+
     void FixedUpdate()
     {
-        // 좌우 이동만 처리
         rb.MovePosition(rb.position + new Vector2(moveX, 0) * moveSpeed * Time.fixedDeltaTime);
     }
 }
